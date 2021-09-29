9c786ce8332119011f246811fc887286e2d37e21
Keely Bailey <keelybailey3@gmail.com>
2021-09-28 22:07:09 -0500

    Start webpack updates

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const config = {
+    entry: "./server.js",
+    output: {
+      path: __dirname + "/dist",
+      filename: "bundle.js"
+    },
+    mode: "development"
+  };
+  
+  module.exports = config;
+  
